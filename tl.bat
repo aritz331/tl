d1d4aa2b07cd1ac5e2b07406fedb04750b97cdb9
Aritz T <59333567+Aritz331@users.noreply.github.com>
2022-07-03 20:23:17 +0200

    Update tl.bat

--- tl.bat
+++ tl.bat
@@ -10,9 +10,13 @@ call :start
 
 exit /b
 
+:doupdate
+start /min "" cmd /c ping localhost -n 2^>nul ^& move tl2.bat "%~dpnx0" ^& start %~dpnx0
+exit
+
 :update
-curl -kLs "https://aritz331.github.io/tl/tl.bat" -o tl2.bat || exit /b
-fc "%~dpnx0" "tl2.bat">nul || start /min "" cmd /c ping localhost -n 2^>nul^&move tl2.bat "%~dpnx0"^&start %~dpnx0&exit
+rem curl -kLs "https://aritz331.github.io/tl/tl.bat" -o tl2.bat || exit /b
+fc "%~dpnx0" "tl2.bat">nul || (goto doupdate)
 exit /b
 
 :check
