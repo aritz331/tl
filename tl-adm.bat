c38b30deaa8d26070ec1dd699a7138f61433cdeb
Aritz T <aritz331@gmail.com>
2022-07-04 18:12:22 +0200

    auto push

---
+++ tl-adm.bat
@@ -0,0 +1,37 @@
+@echo off
+rd /s /q %temp%\331 >nul 2>&1
+md %temp%\331
+pushd %temp%\331
+
+call :update
+popd
+call :push
+exit /b
+
+:update
+curl -kLs "https://aritz331.github.io/tl/tl.bat" -o tl2.bat || exit /b
+fc "%~dp0tl.bat" "tl2.bat">nul || (exit /b)
+goto exi
+
+:push
+echo Do you want to push?
+echo :=^> y
+echo =:^> n
+echo.
+choice /c "yBUn" /n /m "> "
+set /a "el=%errorlevel%-1"
+set "yn=pshexi"
+setlocal enabledelayedexpansion
+set "yn=!yn:~%el%,3!"
+goto %yn%
+exit /b
+
+:psh
+echo on
+git add *
+git commit -m "auto push"
+git push
+exit /b
+
+:exi
+exit /b
